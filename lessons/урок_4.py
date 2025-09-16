127d402673754452cf3590ece0275d7695ca69da
ekokonbaev <ekokonbaev9@gmail.com>
2025-09-16 12:43:36 +0600

    урок4

---
+++ lessons/урок_4.py
@@ -0,0 +1,21 @@
+"""Множество наследование"""
+#Родительский класс
+class A:
+    def action(self):
+        print("A")
+#Наследование
+class B(A):
+    def action(self):
+        print("B")
+#Наследование
+class C(B):
+    def action(self):
+        print("C")
+#Наследование
+class D(C):
+    def action(self):
+        super().action()        #Обращение к С
+        print("D")
+
+obj = D()                          #Обращение классу
+obj.action()                        #Вывод
